19467adf63de7125fbedfe442fd1716d748a392a
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-02-11 17:40:17 +0530

    app.py updated

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -7,3 +8,6 @@ def message():
     return {"message":"Hello World"}
 
 
+if __name__ == "__main__":
+    
+    uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=True)
